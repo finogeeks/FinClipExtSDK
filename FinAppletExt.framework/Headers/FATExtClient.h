3916530d553f402a37fd99de4970df50cce5415b
wanghualei <wanghualei@finogeeks.com>
2020-11-13 16:41:32 +0800

    update sdk to 2.12.39

--- FinAppletExt.framework/Headers/FATExtClient.h
+++ FinAppletExt.framework/Headers/FATExtClient.h
@@ -6,13 +6,19 @@
 //  Copyright © 2020 finogeeks. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
 @interface FATExtClient : NSObject
 
 + (instancetype)sharedClient;
 
 - (void)fat_prepareExtensionApis;
+
+/// 获取webView
+/// @param frame frame
+/// @param URL 网页的URL
+/// @param appletId 小程序ID
+- (UIView *)webViewWithFrame:(CGRect)frame URL:(NSURL *)URL appletId:(NSString *)appletId;
 
 @end
 
