2d1cc5fa6516e643e10fa84ae7c3c66a50fb972c
developer <developer@finogeeks.com>
2022-01-05 09:42:50 +0800

    version++

---
+++ FinAppletExt.framework/Headers/UIColor+FATExtExtension.h
@@ -0,0 +1,19 @@
+//
+//  UIColor+FATExtExtension.h
+//  FinAppletExt
+//
+//  Created by 王兆耀 on 2021/10/18.
+//  Copyright © 2021 finogeeks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UIColor (FATExtExtension)
+
++ (UIColor *)fat_ext_colorWithHexString:(NSString *)hexColor;
+
+@end
+
+NS_ASSUME_NONNULL_END
